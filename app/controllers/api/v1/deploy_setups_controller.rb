2d909f9099a66fd8a90b22305f9e2149ec0c5bd3
Javier Caballero <Javier.X.Caballero.-ND@disney.com>
2018-07-06 17:08:41 -0300

    Deploy setup created

---
+++ app/controllers/api/v1/deploy_setups_controller.rb
@@ -0,0 +1,25 @@
+class Api::V1::DeploySetupController < Api::V1::BaseController
+  def index
+    respond_with App.all
+  end
+
+  def create
+    respond_with :api, :v1, App.create(app_params)
+  end
+
+  def destroy
+    respond_with App.destroy(params[:id])
+  end
+
+  def update
+    app_item = App.find(params["id"])
+    app_item.update_attributes(app_params)
+    respond_with app_item, json: app_item
+  end
+
+  private
+
+    def app_params
+      params.require(:app).permit(:id, :name, :description, :docker_image, :exposed_ports)
+    end
+end
