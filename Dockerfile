d6bdff8f6032ec8dd9405004f6f98c7becfe15fb
Javier Hernán Caballero García <caballerojavier13@gmail.com>
2018-08-20 02:52:01 -0300

    Docker build configuration

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM ruby:2.3.1
+RUN apt-get update -qq && apt-get install -y build-essential nodejs
+RUN mkdir /myapp
+
+ENV RAILS_ENV=production
+
+WORKDIR /myapp
+COPY Gemfile /myapp/Gemfile
+COPY Gemfile.lock /myapp/Gemfile.lock
+RUN bundle install
+COPY . /myapp
+RUN rake assets:precompile
+
+EXPOSE 3000
+
+CMD bundle exec rails s -p 3000 -b '0.0.0.0'
--- app/controllers/api/v1/deploys_controller.rb
+++ app/controllers/api/v1/deploys_controller.rb
@@ -1,7 +1,7 @@
+# frozen_string_literal: true
+
 class Api::V1::DeploysController < Api::V1::BaseController
-
   def index
-
     result = Deploy.all
 
     render json: result, include: [
@@ -10,59 +10,51 @@ class Api::V1::DeploysController < Api::V1::BaseController
   end
 
   def show
-    render json: Deploy.find(params["id"]), include: [
-      :environment,
-      :app,
-      :app_version,
-      :deploy_app_environment_var]
+    render json: Deploy.find(params['id']), include: %i[
+      environment
+      app
+      app_version
+      deploy_app_environment_var
+    ]
   end
 
   def create
-
     deploy_item = Deploy.new(deploy_params)
 
-    exist_others_deploy_in_progress = Deploy.where(
+    exist_others_deploy_in_progress = !Deploy.where(
       status: 'in_progress',
       environment_id: deploy_params[:environment_id]
-    ).size > 0
+    ).empty?
 
     deploy_item.status = exist_others_deploy_in_progress || deploy_params[:status] == 'draft' ? 'draft' : 'in_progress'
 
-    deploy_item.name = Environment.find(deploy_params[:environment_id]).name + '#'  + DateTime.now.strftime('%Y%m%d%H%M%S')
-
-    deploy_apps = []
+    deploy_item.name = Environment.find(deploy_params[:environment_id]).name + '#' + DateTime.now.strftime('%Y%m%d%H%M%S')
 
     deploy_item.save
 
     params[:deploy][:deploy_apps].each do |_, da|
-
       deploy_app_item = DeployApp.new(da.permit(:app_id, :app_version_id, :deploy_setup_id))
       deploy_app_item.deploy = deploy_item
       deploy_app_item.container_name = App.find(da[:app_id]).name
       deploy_app_item.restart_policy = da[:deploy_setup][:restart_policy]
       deploy_app_item.ports = da[:deploy_setup][:ports]
 
-      deploy_app_item.save()
+      deploy_app_item.save
 
       deploy_app_item.set_deploy_app_env_vars(da[:deploy_setup_id])
 
-      deploy_app_item.save()
-
+      deploy_app_item.save
     end
 
-    if deploy_item.status != 'draft'
-      deploy_item.delay.perform_deploy
-    end
+    deploy_item.delay.perform_deploy if deploy_item.status != 'draft'
 
     render json:
-             deploy_item.to_json(:include => [:environment, :deploy_app]),
+             deploy_item.to_json(include: %i[environment deploy_app]),
            status: exist_others_deploy_in_progress ? 400 : 201
-
   end
 
   def destroy
-
-    deploy_item = Deploy.find(params["id"])
+    deploy_item = Deploy.find(params['id'])
 
     if deploy_item.status == 'draft'
       Deploy.find(params[:id]).delete
@@ -70,29 +62,26 @@ class Api::V1::DeploysController < Api::V1::BaseController
     else
       render json: nil, status: 400
     end
-
   end
 
   def update
+    deploy_item = Deploy.find(params[:deploy]['id'])
 
-    deploy_item = Deploy.find(params[:deploy]["id"])
-
-    exist_others_deploy_in_progress = Deploy.where(
+    exist_others_deploy_in_progress = !Deploy.where(
       status: 'in_progress',
-      environment_id: params[:deploy][:environment_id]["$oid"]
-    ).size > 0
+      environment_id: params[:deploy][:environment_id]['$oid']
+    ).empty?
 
     deploy_item.status = exist_others_deploy_in_progress || deploy_params[:status] == 'draft' ? 'draft' : 'in_progress'
 
-    deploy_item.environment = Environment.find(params[:deploy][:environment_id]["$oid"])
+    deploy_item.environment = Environment.find(params[:deploy][:environment_id]['$oid'])
 
     deploy_item.save
 
-    deploy_item.deploy_app.each { |da| da.delete }
+    deploy_item.deploy_app.each(&:delete)
 
     params[:deploy][:deploy_apps].each do |_, da|
-
-      app = App.find(da[:app_id]["$oid"])
+      app = App.find(da[:app_id]['$oid'])
 
       deploy_app_item = DeployApp.new(da.permit(:app_id, :app_version_id, :deploy_setup_id))
       deploy_app_item.deploy = deploy_item
@@ -101,27 +90,23 @@ class Api::V1::DeploysController < Api::V1::BaseController
       deploy_app_item.restart_policy = da[:deploy_setup][:restart_policy]
       deploy_app_item.ports = da[:deploy_setup][:ports]
 
-      deploy_app_item.save()
+      deploy_app_item.save
 
       deploy_app_item.set_deploy_app_env_vars(da[:deploy_setup_id])
 
-      deploy_app_item.save()
-
+      deploy_app_item.save
     end
 
-    if deploy_item.status != 'draft'
-      deploy_item.delay.perform_deploy
-    end
+    deploy_item.delay.perform_deploy if deploy_item.status != 'draft'
 
     render json:
-             deploy_item.to_json(:include => [:environment, :deploy_app]),
+             deploy_item.to_json(include: %i[environment deploy_app]),
            status: exist_others_deploy_in_progress ? 400 : 200
-
   end
 
   private
 
-    def deploy_params
-      params.require(:deploy).permit(:name, :environment_id, :status)
-    end
+  def deploy_params
+    params.require(:deploy).permit(:name, :environment_id, :status)
+  end
 end
