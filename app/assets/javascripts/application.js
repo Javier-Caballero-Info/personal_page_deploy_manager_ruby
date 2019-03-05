0210692e6924ad99c683893e0708b8a6d1e1bb17
Javier Hernán Caballero García <caballerojavier13@gmail.com>
2019-03-05 20:15:46 -0300

    Solving little issue javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -34,9 +34,9 @@ let notification = {
 
 function readCookie(name) {
     let nameEQ = name + "=";
-    const ca = document.cookie.split(';');
+    let ca = document.cookie.split(';');
     for (let i = 0; i < ca.length; i++) {
-        const c = ca[i];
+        let c = ca[i];
         while (c.charAt(0) == ' ') c = c.substring(1, c.length);
         if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
     }
