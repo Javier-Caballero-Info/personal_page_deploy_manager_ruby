57832af5c33781939e29084c6d83ff260468bb20
Javier Hernán Caballero García <caballerojavier13@gmail.com>
2019-03-05 21:45:22 -0300

    Fixing cookie domain for deoployer

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 Rails.application.config.session_store :cookie_store, domain: {
-      production: '.javiercaballero.info',
+      production: 'deployer.javiercaballero.info',
       development: 'localhost'
   }.fetch(Rails.env.to_sym, :all)
