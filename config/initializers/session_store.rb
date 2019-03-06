91a6599be4eba08cd536f4f8feb115121f345ab0
Javier Hernán Caballero García <caballerojavier13@gmail.com>
2019-03-05 22:00:57 -0300

    Remove domain in cookies

--- config/initializers/session_store.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.config.session_store :cookie_store, domain: {
-      production: 'deployer.javiercaballero.info',
-      development: 'localhost'
-  }.fetch(Rails.env.to_sym, :all)
